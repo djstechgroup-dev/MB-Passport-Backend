a3706ffe96a28ca733b68706331a0e4d6142b1ab
jayeshjain25 <jayeshkjain6@gmail.com>
2022-07-30 00:05:18 +0530

    updated backend

---
+++ routes/authRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+
+const {signup, signin, signupMobile, signinMobile} = require("../controller/auth")
+
+router.post('/signup', signup)
+router.post('/mobile_signup', signupMobile)
+router.post('/mobile_signin', signinMobile)
+router.post('/signin', signin)
+
+module.exports = router
